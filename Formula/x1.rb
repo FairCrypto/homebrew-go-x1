6cf07df6a419f201643212817855739f1fcc46b3
Nicholas Pettas <npettas@gmail.com>
2024-01-17 18:40:17 -0800

    update to v0.9.0-4

--- Formula/x1.rb
+++ Formula/x1.rb
@@ -2,15 +2,30 @@ class X1 < Formula
   desc "X1 blockchain protocol secured by the Lachesis consensus algorithm"
   homepage "https://xen.network"
   license "LGPL-3.0"
-  url "https://github.com/FairCrypto/go-x1/archive/refs/tags/v0.9.0-1.tar.gz"
-  sha256 "b230495c3fdd2d6ebed92ac8b833acb3cd62a949b5decf7b8330718468fb8f50"
+  url "https://github.com/FairCrypto/go-x1/archive/refs/tags/v0.9.0-4.tar.gz"
+  sha256 "ca4e1332035c6b19f3f8c2eaec0b197462a5e7a4c1768e345b31378f18ca9022"
 
   depends_on "go" => :build
   depends_on "make" => :build
 
+  service do
+    run [opt_bin/"x1", "--config", "#{etc}/x1/config.toml"]
+    keep_alive true
+    environment_variables PATH: HOMEBREW_PREFIX/"bin"
+    log_path var/"log/x1.log"
+    error_log_path var/"log/x1.log"
+  end
+
   def install
     system "make", "x1"
     bin.install Dir["build/*"]
+    mkdir_p "#{etc}/x1"
+    etc.install "system/etc/x1/config.toml" => "x1/config.toml"
+    mkdir_p "#{share}/x1/configs/testnet"
+    share.install "system/usr/share/x1/configs/testnet/api-node.toml" => "x1/configs/testnet/api-node.toml"
+    share.install "system/usr/share/x1/configs/testnet/full-node.toml" => "x1/configs/testnet/full-node.toml"
+    share.install "system/usr/share/x1/configs/testnet/archive-node.toml" => "x1/configs/testnet/archive-node.toml"
+    share.install "system/usr/share/x1/configs/testnet/validator-node.toml" => "x1/configs/testnet/validator-node.toml"
   end
 
   test do
