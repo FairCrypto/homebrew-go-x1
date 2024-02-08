10c235579cd67168d99979597cc63ac5b1223b07
Nicholas Pettas <npettas@gmail.com>
2024-02-08 15:05:37 -0800

    update to v1.1.5-rc.3

--- Formula/x1.rb
+++ Formula/x1.rb
@@ -1,8 +1,8 @@
 class X1 < Formula
   desc "Official Go implementation of the X1 protocol"
   homepage "https://xen.network"
-  url "https://github.com/FairCrypto/go-x1/archive/refs/tags/v1.1.5-rc.1.tar.gz"
-  sha256 "241c6b53f855da22993085eacce9883c6201c7ecb6d76e07fd88a7ec0f0bccc2"
+  url "https://github.com/FairCrypto/go-x1/archive/refs/tags/v1.1.5-rc.3.tar.gz"
+  sha256 "b2f64365b455b5125a7dc0f9a22a36c0e2a85a063fb7117fc08b8ac9a680e8d0"
   license "LGPL-3.0"
 
   depends_on "go" => :build
